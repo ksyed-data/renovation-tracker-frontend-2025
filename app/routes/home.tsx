a4a4efed2fc4b2ac35a2854b2a94b78ce4a7ad17
Steven Zheng <zhengs7@vcu.edu>
2025-10-14 15:06:13 -0400

    rebuild using react router

---
+++ app/routes/home.tsx
@@ -0,0 +1,14 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../components/layouts/welcome/welcome";
+import { Dashboard } from "~/components/layouts/dashboard";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <Dashboard />;
+}
