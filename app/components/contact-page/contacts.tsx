ac5ffa6e3a9cb1bd17d0bdadb10ca6b28296ddc8
Victoria Castagnola <castagnola.victoria@gmail.com>
2026-02-13 14:26:00 -0500

    added basic info and names

---
+++ app/components/contact-page/contacts.tsx
@@ -0,0 +1,35 @@
+// app/components/contact-page/contacts.tsx
+//working on editing text descriptions as best I can and making sure page has good basic functionality
+//then will prioritize styling and making things look nice
+export const Contacts = () => {
+  return (
+    <div className="flex justify-center py-3">
+      <div className="py-3">
+        <h1 className="text-gray-800 text-4xl p-3 font-bold text-center">
+          Contacts
+        </h1>
+        <p className="text-gray-500 max-w-250 text-2xl text-center">
+          Victoria Castagnola{" "}
+        </p>
+        <p className="text-gray-500 max-w-250 text-2xl text-center">
+          Angela Pantigozo-Cuellar{" "}
+        </p>
+        <p className="text-gray-500 max-w-250 text-2xl text-center">
+          Steven Zheng{" "}
+        </p>
+        <p className="text-gray-500 max-w-250 text-2xl text-center">
+          Jeremiah Robert{" "}
+        </p>
+        <p className="text-gray-500 max-w-250 text-2xl text-center">
+          Jin Lee{" "}
+        </p>
+        <p className="text-gray-500 max-w-250 text-2xl text-center">
+          Justin Jones{" "}
+        </p>
+        <p className="text-gray-500 max-w-250 text-2xl text-center">
+          Abigail Marlett{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
