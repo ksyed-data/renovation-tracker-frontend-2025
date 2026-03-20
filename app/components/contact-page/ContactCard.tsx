e90cf3e52d797364340f5766ad4eca4416bfbcb5
Victoria Castagnola <castagnola.victoria@gmail.com>
2026-03-20 13:36:49 -0400

    added profile images

--- app/components/contact-page/ContactCard.tsx
+++ app/components/contact-page/ContactCard.tsx
@@ -8,8 +8,16 @@ interface ContactCardProps {
 export function ContactCard({ contact }: ContactCardProps) {
   return (
     <div className="bg-white rounded-lg shadow-md p-6 text-center flex flex-col items-center">
-      {/* Avatar Placeholder */}
-      <User className="w-20 h-20 text-gray-400" />
+      {/* Avatar */}
+      {contact.photo ? (
+        <img
+          src={contact.photo}
+          alt={`${contact.name} avatar`}
+          className="w-20 h-20 rounded-full object-cover"
+        />
+      ) : (
+        <User className="w-20 h-20 text-gray-400" />
+      )}
       <h3 className="font-semibold text-lg">{contact.name}</h3>
       <p className="text-sm text-gray-600">{contact.role}</p>
       <p className="text-sm text-gray-600">{contact.organization}</p>
--- app/components/contact-page/contacts.ts
+++ app/components/contact-page/contacts.ts
@@ -5,16 +5,18 @@ export interface Contact {
   linkedin?: string;
   phone?: string;
   email: string;
+  photo?: string;
 }
 
 export const contactList: Contact[] = [
   {
     name: "Victoria Castagnola",
-    role: "Project Manager & Front-End Developer",
+    role: "Project Manager",
     organization: "Virginia Commonwealth University",
     linkedin: "https://www.linkedin.com/in/victoria-castagnola-vcu/",
     phone: "540-554-1155",
     email: "castagnola.victoria@gmail.com",
+    photo: "https://media.licdn.com/dms/image/v2/D4E03AQE_yMzSb2xPbw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1726255580478?e=1775692800&v=beta&t=AQJw2sFXuCjbRAhqyqcRg72rKT-Df_B607Stlk9HVWQ"
   },
   {
     name: "Jin Lee",
@@ -22,7 +24,7 @@ export const contactList: Contact[] = [
     organization: "Virginia Commonwealth University",
     linkedin: "https://www.linkedin.com/in/jin-lee-03m0805/",
     email: "leej116@vcu.edu",
-    
+    photo: "https://media.licdn.com/dms/image/v2/D5603AQG68Cl_vPU_sQ/profile-displayphoto-scale_400_400/B56ZlVsdrMIsAg-/0/1758079343761?e=1775692800&v=beta&t=Oy0V2SkuSrHwTB7RREFC9uIAZjgM9Gw3cngLSAnckqs"
   },
   {
     name: "Steven Zheng",
@@ -31,6 +33,7 @@ export const contactList: Contact[] = [
     linkedin: "https://www.linkedin.com/in/zheng-steven/",
     phone: "540-554-1157",
     email: "steven.zheng7@icloud.com ",
+    photo: "https://media.licdn.com/dms/image/v2/D4D35AQE6PVyo2GiIiQ/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1646280521593?e=1774634400&v=beta&t=mjjr8XsuZxKWcnePSSw1JywbmBKUWNJBfiZYR1xaUmo"
   },
   {
     name: "Angela Pantigozo-Cuellar",
@@ -38,6 +41,7 @@ export const contactList: Contact[] = [
     organization: "Virginia Commonwealth University",
     linkedin: "https://www.linkedin.com/in/angela-pantigozo-cuellar/",
     email: "pantigozocam@vcu.edu",
+    photo: "https://media.licdn.com/dms/image/v2/D4D03AQG_OKUDP0ZncQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1713996147784?e=1775692800&v=beta&t=_271RzZBxq9rWLgkNJn4B8PQGz3sS3vvKZXPNsi1coQ"
   },
   {
     name: "Jeremiah Robert",
@@ -45,6 +49,7 @@ export const contactList: Contact[] = [
     organization: "Virginia Commonwealth University",
     linkedin: "https://www.linkedin.com/in/jeremiah-robert/",
     email: "jeremiadrjohn@gmail.com",
+    photo: "https://media.licdn.com/dms/image/v2/D4E03AQEhFXJeWB1ixw/profile-displayphoto-crop_800_800/B4EZl0WYOGHIAI-/0/1758593647795?e=1775692800&v=beta&t=zBSHKxXzfZW_gtyudHJBGfx000M3IkmUG8h1LRZLoJ4"
   },
   {
     name: "Justin Jones",
@@ -52,6 +57,7 @@ export const contactList: Contact[] = [
     organization: "CoStar Group",
     linkedin: "https://www.linkedin.com/in/jonesjt23/",
     email: "jjones@costar.com",
+    photo: "https://media.licdn.com/dms/image/v2/C5603AQGA63VK9J7HkQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1645116338018?e=1775692800&v=beta&t=qMbPGJ-65rCGWVUuk_sB9ezmvqPaNspKNbLuodFQzHU"
   },
   {
     name: "Abigail Marlett",
@@ -59,13 +65,15 @@ export const contactList: Contact[] = [
     organization: "CoStar Group",
     linkedin: "https://www.linkedin.com/in/abigail-marlett/",
     email: "amarlett@costar.com",
+    photo: "https://media.licdn.com/dms/image/v2/D5603AQEEI1c6ya-wLA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1694636198798?e=1775692800&v=beta&t=IIKd27H-MAyjMGczzbuglCrI_Coue9abAY69UZ17qqw"
   },
   {
     name: "Dr. Syed Khaja Moinuddin",
-    role: "Co",
+    role: "Company Advisor",
     organization: "CoStar Group",
     linkedin: "https://www.linkedin.com/in/syed-khajamoinuddin/",
     email: "ksyed@costar.com",
+    photo: "https://media.licdn.com/dms/image/v2/C4D03AQEX8E7uV06YUw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1522428120798?e=1775692800&v=beta&t=uFTXwlym75A8qou5SBnMLcplECST37JWx0Tb-Oi23Oc"
   },
   {
     name: "Dr. Preetam Ghosh",
@@ -73,6 +81,7 @@ export const contactList: Contact[] = [
     organization: "Virginia Commonwealth University",
     linkedin: "https://www.linkedin.com/in/preetam-ghosh-9a1b8a1a/",
     email: "pghosh@vcu.edu",
+    photo: "https://media.licdn.com/dms/image/v2/C5603AQExlRZ6ky4qDw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1516248780064?e=1775692800&v=beta&t=cXyUdjgcNhS8RwJNZa0O98aWfnsKt-m9Sb-WmIQpJHk"
   }
 
 ];
